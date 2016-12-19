278440048d69733904f44cdcbc9ed4dcb03a49bb
Jed <clampett@m6700.lan>
2016-12-19 16:22:56 -0500

    Downloaded Scripts

---
+++ m-scripts/AircraftPitch/AircraftPitch_ControlDigital.m
@@ -0,0 +1,138 @@
+%% Index: Control Tutorials for MATLAB & Simulink
+%
+%% Introduction
+%
+% System: 
+% < ?example=Introduction&section=SystemModeling Modeling> |
+% < ?example=Introduction&section=SystemAnalysis Analysis>
+%
+% Control: 
+% < ?example=Introduction&section=ControlPID PID> |
+% < ?example=Introduction&section=ControlRootLocus Root Locus> |
+% < ?example=Introduction&section=ControlFrequency Frequency> |
+% < ?example=Introduction&section=ControlStateSpace State-Space> |
+% < ?example=Introduction&section=ControlDigital Digital>
+%
+% Simulink: 
+% < ?example=Introduction&section=SimulinkModeling Modeling> |
+% < ?example=Introduction&section=SimulinkControl Control>
+%
+%% Cruise Control
+%
+% System: 
+% < ?example=CruiseControl&section=SystemModeling Modeling> |
+% < ?example=CruiseControl&section=SystemAnalysis Analysis>
+%
+% Control: 
+% < ?example=CruiseControl&section=ControlPID PID> |
+% < ?example=CruiseControl&section=ControlRootLocus Root Locus> |
+% < ?example=CruiseControl&section=ControlFrequency Frequency> |
+% < ?example=CruiseControl&section=ControlStateSpace State-Space> |
+% < ?example=CruiseControl&section=ControlDigital Digital>
+%
+% Simulink: 
+% < ?example=CruiseControl&section=SimulinkModeling Modeling> |
+% < ?example=CruiseControl&section=SimulinkControl Control>
+%
+%% DC Motor Speed
+%
+% System: 
+% < ?example=MotorSpeed&section=SystemModeling Modeling> |
+% < ?example=MotorSpeed&section=SystemAnalysis Analysis>
+%
+% Control: 
+% < ?example=MotorSpeed&section=ControlPID PID> |
+% < ?example=MotorSpeed&section=ControlRootLocus Root Locus> |
+% < ?example=MotorSpeed&section=ControlFrequency Frequency> |
+% < ?example=MotorSpeed&section=ControlStateSpace State-Space> |
+% < ?example=MotorSpeed&section=ControlDigital Digital>
+%
+% Simulink: 
+% < ?example=MotorSpeed&section=SimulinkModeling Modeling> |
+% < ?example=MotorSpeed&section=SimulinkControl Control>
+%
+%% DC Motor Position
+%
+% System: 
+% < ?example=MotorPosition&section=SystemModeling Modeling> |
+% < ?example=MotorPosition&section=SystemAnalysis Analysis>
+%
+% Control: 
+% < ?example=MotorPosition&section=ControlPID PID> |
+% < ?example=MotorPosition&section=ControlRootLocus Root Locus> |
+% < ?example=MotorPosition&section=ControlFrequency Frequency> |
+% < ?example=MotorPosition&section=ControlStateSpace State-Space> |
+% < ?example=MotorPosition&section=ControlDigital Digital>
+%
+% Simulink: 
+% < ?example=MotorPosition&section=SimulinkModeling Modeling> |
+% < ?example=MotorPosition&section=SimulinkControl Control>
+%
+%% Suspension
+%
+% System: 
+% < ?example=Suspension&section=SystemModeling Modeling> |
+% < ?example=Suspension&section=SystemAnalysis Analysis>
+%
+% Control: 
+% < ?example=Suspension&section=ControlPID PID> |
+% < ?example=Suspension&section=ControlRootLocus Root Locus> |
+% < ?example=Suspension&section=ControlFrequency Frequency> |
+% < ?example=Suspension&section=ControlStateSpace State-Space> |
+% < ?example=Suspension&section=ControlDigital Digital>
+%
+% Simulink: 
+% < ?example=Suspension&section=SimulinkModeling Modeling> |
+% < ?example=Suspension&section=SimulinkControl Control>
+%
+%% Inverted Pendulum
+%
+% System: 
+% < ?example=InvertedPendulum&section=SystemModeling Modeling> |
+% < ?example=InvertedPendulum&section=SystemAnalysis Analysis>
+%
+% Control: 
+% < ?example=InvertedPendulum&section=ControlPID PID> |
+% < ?example=InvertedPendulum&section=ControlRootLocus Root Locus> |
+% < ?example=InvertedPendulum&section=ControlFrequency Frequency> |
+% < ?example=InvertedPendulum&section=ControlStateSpace State-Space> |
+% < ?example=InvertedPendulum&section=ControlDigital Digital>
+%
+% Simulink: 
+% < ?example=InvertedPendulum&section=SimulinkModeling Modeling> |
+% < ?example=InvertedPendulum&section=SimulinkControl Control>
+%
+%% Aircraft Pitch
+%
+% System: 
+% < ?example=AircraftPitch&section=SystemModeling Modeling> |
+% < ?example=AircraftPitch&section=SystemAnalysis Analysis>
+%
+% Control: 
+% < ?example=AircraftPitch&section=ControlPID PID> |
+% < ?example=AircraftPitch&section=ControlRootLocus Root Locus> |
+% < ?example=AircraftPitch&section=ControlFrequency Frequency> |
+% < ?example=AircraftPitch&section=ControlStateSpace State-Space> |
+% < ?example=AircraftPitch&section=ControlDigital Digital>
+%
+% Simulink: 
+% < ?example=AircraftPitch&section=SimulinkModeling Modeling> |
+% < ?example=AircraftPitch&section=SimulinkControl Control>
+%
+%% Ball & Beam
+%
+% System: 
+% < ?example=BallBeam&section=SystemModeling Modeling> |
+% < ?example=BallBeam&section=SystemAnalysis Analysis>
+%
+% Control: 
+% < ?example=BallBeam&section=ControlPID PID> |
+% < ?example=BallBeam&section=ControlRootLocus Root Locus> |
+% < ?example=BallBeam&section=ControlFrequency Frequency> |
+% < ?example=BallBeam&section=ControlStateSpace State-Space> |
+% < ?example=BallBeam&section=ControlDigital Digital>
+%
+% Simulink: 
+% < ?example=BallBeam&section=SimulinkModeling Modeling> |
+% < ?example=BallBeam&section=SimulinkControl Control>
+%
