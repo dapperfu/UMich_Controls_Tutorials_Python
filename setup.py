1008681ba951a7d9d3c4542121a75ad2afcf32d4
jed <jed+python-controls@m6700-LINUX>
2018-09-30 22:34:57 -0400

    .

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+import versioneer
+
+import os
+
+LOCAL_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def read_requirements(path="requirements.txt"):
+    """Read requirements file relative to setup.py"""
+    full_path = os.path.join(LOCAL_DIR, path)
+    if not os.path.exists(full_path):
+        return []
+    
+    def yield_line(path):
+        with open(path, "r") as fid:
+            for line in fid.readlines():
+                yield line
+
+    return [requirement.strip() for requirement in yield_line(full_path) if not requirement.startswith("#")]
+
+requirements = read_requirements()
+print(requirements)
+#test_requirements = read_requirements(path="requirements_test.txt")
+
+setuptools.setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    name="UMich Controls - Python",
+    description="GCode",
+    license="BSD",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=[],
+    include_package_data=True,
+)
